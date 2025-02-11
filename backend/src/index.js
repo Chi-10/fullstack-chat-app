e6f1100b779c6edf5cc41b3b134a2d5812bca1cb
Chiedu <chiedu4422@gmail.com>
2025-02-11 17:18:41 +0100

    adjustment to the index.js file for the backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,42 +1,42 @@
-import express from "express"
-import dotenv from 'dotenv'
-import cookieParser from "cookie-parser"
-import cors from "cors"
-
+import express from "express";
+import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url"; // Import the method to convert URL to path
 
-import {connectDB} from './lib/db.js'
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.route.js"
-import { app, server } from "./lib/socket.js"
+import { connectDB } from './lib/db.js';
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
+dotenv.config();
 
-dotenv.config()
+const PORT = process.env.PORT || 5001;
 
-
-const PORT = process.env.PORT || 5001
+// Emulate __dirname in ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
+}));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
 }
-))
 
-app.use("/api/auth", authRoutes)
-app.use("/api/messages", messageRoutes)
-
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
-
-    app.get("*", (req,res)=>{
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
-    })
-}
-
-server.listen(PORT, ()=>{
+server.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`);
-    connectDB()
-    
-})
+    connectDB();
+});
